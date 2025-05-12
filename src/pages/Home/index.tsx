78e0fbffa3677c2e2c6228fa65f76090a0555ce5
Juan Pina <juanpinaribeiro@outlook.com>
2025-05-12 11:07:05 -0300

    Funcionamento: reduzindo o countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from "zod";
 
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, "Informe a tarefa."),
@@ -17,6 +18,7 @@ interface Cycle {
     id: string
     task: string
     minutesAmount: number
+    startDate: Date
 }
 
 export function Home() {
@@ -25,6 +27,18 @@ export function Home() {
     const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
     const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+    useEffect(() => {
+        if (activeCycle) {
+            setInterval(() => {
+                setAmountSecondsPassed(
+                    differenceInSeconds(new Date(), activeCycle.startDate),
+                )
+            }, 1000)
+        }
+    }, [activeCycle])
+
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
@@ -32,8 +46,6 @@ export function Home() {
             minutesAmount: 0,
         }
     })
-
-    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -46,6 +58,7 @@ export function Home() {
 
     const task = watch("task");
     const isSubmitDisabled = !task
+    
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         const id = String(new Date().getTime());
@@ -54,6 +67,7 @@ export function Home() {
             id: id,
             task: data.task,
             minutesAmount: data.minutesAmount,
+            startDate: new Date(),
         }
 
         setCycles(state => [...state, newCycle])
